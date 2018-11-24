f172cc0d22dea50ccc6bf63489959f988c1d9feb
Unk9vvN <40751222+unk9vvn@users.noreply.github.com>
2018-11-24 18:40:10 -0500

    Update atj.py

--- atj.py
+++ atj.py
@@ -101,7 +101,7 @@ class atj:
 
     @staticmethod
     def Tor(self):
-        socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050, True)
+        socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS4, "127.0.0.1", 9050, True)
         socket.socket = socks.socksocket
 
 
