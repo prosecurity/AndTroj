ee0ce2879aec191ba58179685696bdd65745e319
Unk9vvN <40751222+unk9vvn@users.noreply.github.com>
2018-11-27 19:39:57 -0500

    Update update.py

--- core/update.py
+++ core/update.py
@@ -9,7 +9,7 @@ dir = '/usr/share/AndTroj'
 
 def updater():
     global checker, state
-    __version__ = '2.3.0'
+    __version__ = '2.0.0'
     last_ver = 'https://raw.githubusercontent.com/unk9vvn/AndTroj/master/others/version'
 
     state = 0
@@ -28,7 +28,6 @@ def updater():
 
         if update is True:
             print "Download new version: AndTroj-{0}.tar.gz".format(checker)
-            cls()
             try:
                 subprocess.call(
                     'rm -r /usr/share/AndTroj && mkdir /usr/share/AndTroj',
@@ -37,11 +36,13 @@ def updater():
                     'wget ' + updater + ' -O /tmp/atj_update.tar.gz',
                     shell=True)
                 subprocess.call(
-                    'tar -xvf /tmp/atj_update.tar.gz && cd /tmp/AndTroj-' + checker + ' && cp -r * /usr/share/AndTroj/',
+                    'cd /tmp && tar -xvf atj_update.tar.gz && cd /tmp/AndTroj-' + checker + ' && cp -r * /usr/share/AndTroj/',
                     shell=True)
                 subprocess.call(
                     'rm /tmp/atj_update.tar.gz && rm -r /tmp/AndTroj-' + checker,
                     shell=True)
+                cls()
                 print "Updated: new version {0}".format(checker)
             except:
+                cls()
                 print "Connection Error!\n\n"
