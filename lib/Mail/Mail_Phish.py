c2e8b3ac6014e7de87b381c3b501de231153ba0d
Unk9vvN <40751222+unk9vvn@users.noreply.github.com>
2019-02-20 18:15:57 -0500

    Update Mail_Phish.py

--- lib/Mail/Mail_Phish.py
+++ lib/Mail/Mail_Phish.py
@@ -3,7 +3,7 @@
 # https://t.me/unk9vvn
 # AVI
 from core.menu import *
-from core.start import Tor
+
 
 
 dir = '/usr/share/AndTroj'
@@ -22,10 +22,12 @@ bslash = bslash1 + bslash2
 slashkutat = bslash1 + kutation
 
 
+
 LAN = ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2]
                           if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)),
                           s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET,
                           socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+
 
 
 def mailph():
