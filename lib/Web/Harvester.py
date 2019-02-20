4d8f576e6f62b73716d3ec32ba9cd6abffc0b958
Unk9vvN <40751222+unk9vvn@users.noreply.github.com>
2019-02-20 18:17:38 -0500

    Update Harvester.py

--- lib/Web/Harvester.py
+++ lib/Web/Harvester.py
@@ -5,6 +5,7 @@
 from core.menu import *
 
 
+
 LAN = ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2]
                           if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)),
                           s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET,
